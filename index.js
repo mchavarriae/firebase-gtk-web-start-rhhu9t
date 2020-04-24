3b9fba983d33a2bb249c52c3d97cee8cdc9d629c
mchavarriae <marlonc@tevixmd.com>
2020-04-23 20:52:35 -0600

    session 2.. final

--- index.js
+++ index.js
@@ -67,10 +67,12 @@ firebase.auth().onAuthStateChanged(user => {
     startRsvpButton.textContent = "Logout";
     guestbookContainer.style.display = "block";
     subscribeGuestbook();
+    subscribeCurrentRSVP(user);
   } else {
     startRsvpButton.textContent = "RSVP";
     guestbookContainer.style.display = "none";
     unsubscribeGuestbook();
+    unsubscribeCurrentRSVP();
   }
 });
 
@@ -104,9 +106,70 @@ function subscribeGuestbook() {
     });
 }
 
-function unsubscribeGuestbook(){
- if(guestbookListener !== null){
-   guestbookListener();
-   guestbookListener = null;
- }
+function unsubscribeGuestbook() {
+  if (guestbookListener !== null) {
+    guestbookListener();
+    guestbookListener = null;
+  }
+}
+
+rsvpYes.onclick = () => {
+  const userDoc = firebase
+    .firestore()
+    .collection("attendees")
+    .doc(firebase.auth().currentUser.uid);
+  userDoc
+    .set({
+      attending: true
+    })
+    .catch(console.error);
+};
+
+rsvpNo.onclick = () => {
+  const userDoc = firebase
+    .firestore()
+    .collection("attendees")
+    .doc(firebase.auth().currentUser.uid);
+  userDoc
+    .set({
+      attending: false
+    })
+    .catch(console.error);
+};
+
+firebase
+  .firestore()
+  .collection("attendees")
+  .where("attending", "==", true)
+  .onSnapshot(snap => {
+    const newAttendeeCount = snap.docs.length;
+    numberAttending.innerHTML = newAttendeeCount + " peolple going";
+  });
+
+function subscribeCurrentRSVP(user) {
+  rsvpListener = firebase
+    .firestore()
+    .collection("attendees")
+    .doc(user.uid)
+    .onSnapshot(doc => {
+      if (doc != null && doc.data()) {
+        const attendingResponse = doc.data().attending;
+        if (attendingResponse) {
+          rsvpYes.className = "clicked";
+          rsvpNo.className = "";
+        } else {
+          rsvpYes.className = "";
+          rsvpNo.className = "clicked";
+        }
+      }
+    });
+}
+
+function unsubscribeCurrentRSVP(){
+  if(rsvpListener != null){
+    rsvpListener();
+    rsvpListener = null;
+  }
+  rsvpYes.className = "";
+  rsvpNo.className = "";
 }
